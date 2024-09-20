ff8a19c1c178d84c97c68b497d7174027cb8de16
NikolausPetrov <162726334+NikolausPetrov@users.noreply.github.com>
2024-09-20 17:17:12 +0300

    Add files via upload

---
+++ api/Main.java
@@ -0,0 +1,14 @@
+package api;
+
+import service.MyService;
+import db.MyEntity;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
